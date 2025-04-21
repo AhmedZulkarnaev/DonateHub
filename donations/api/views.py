ded494dc72e6e33773d9c89e6ba344dd26c504a7
AhmedZulkarnaev <ahmedzulkarnaev9500@gmail.com>
2025-04-21 22:13:57 +0300

    Исправление бага с id при кэшировании

--- donations/api/views.py
+++ donations/api/views.py
@@ -1,5 +1,5 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from donations.models import Collect, Payment
 from .serializers import CollectSerializer, PaymentSerializer
@@ -9,39 +9,107 @@ from ..constants import CACHE_KEY_PREFIX
 
 class CollectViewSet(ModelViewSet):
     """
-    ViewSet для модели Collect:
-    - Поддерживает полный CRUD через REST API
-    - Только авторизованные пользователи могут создавать/изменять/удалять
-    - Неавторизованные пользователи могут только просматривать
+    ViewSet для работы с денежными сборами (Collect).
+
+    Позволяет:
+    - Просматривать список сборов (GET /collects/)
+    - Создавать новые сборы (POST /collects/)
+    - Просматривать детали сбора (GET /collects/{id}/)
+    - Обновлять сборы (PUT/PATCH /collects/{id}/)
+    - Удалять сборы (DELETE /collects/{id}/)
+
+    Требования:
+    - Аутентификация требуется для создания, обновления и удаления
+    - Анонимные пользователи могут только просматривать
     """
     queryset = Collect.objects.all()
     serializer_class = CollectSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
+    @swagger_auto_schema(
+        operation_description="Получить список всех денежных сборов",
+        responses={200: CollectSerializer(many=True)},
+        tags=['Сборы']
+    )
     @cache_response(key_prefix=CACHE_KEY_PREFIX)
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
-    @cache_response(key_prefix=f"{CACHE_KEY_PREFIX}_single")
+    @swagger_auto_schema(
+        operation_description="Получить детальную информацию о конкретном сборе",
+        responses={
+            200: CollectSerializer(),
+            404: "Сбор не найден"
+        },
+        tags=['Сборы']
+    )
+    @cache_response(
+        key_prefix=lambda view, request, *args, **kwargs:
+        f"{CACHE_KEY_PREFIX}_single_{kwargs.get('pk') or kwargs.get('id')}")
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Создать новый денежный сбор",
+        request_body=CollectSerializer,
+        responses={
+            201: CollectSerializer(),
+            400: "Неверные данные"
+        },
+        tags=['Сборы'],
+        security=[{'Bearer': []}]
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
 
 class PaymentViewSet(ModelViewSet):
     """
-    ViewSet для модели Payment:
-    - Поддерживает CRUD операции для платежей
-    - Только авторизованные пользователи могут отправлять платежи
-    - Неавторизованные пользователи могут только просматривать
+    ViewSet для работы с платежами (Payment).
+
+    Позволяет:
+    - Просматривать список платежей (GET /payments/)
+    - Создавать новые платежи (POST /payments/)
+    - Просматривать детали платежа (GET /payments/{id}/)
+    - Обновлять платежи (PUT/PATCH /payments/{id}/)
+    - Удалять платежи (DELETE /payments/{id}/)
+
+    Требования:
+    - Аутентификация требуется для создания платежей
+    - Анонимные пользователи могут только просматривать
     """
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
+    @swagger_auto_schema(
+        operation_description="Получить список всех платежей",
+        responses={200: PaymentSerializer(many=True)},
+        tags=['Платежи']
+    )
     @cache_response(key_prefix=CACHE_KEY_PREFIX)
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
+    @swagger_auto_schema(
+        operation_description="Получить детальную информацию о конкретном платеже",
+        responses={
+            200: PaymentSerializer(),
+            404: "Платеж не найден"
+        },
+        tags=['Платежи']
+    )
     @cache_response(key_prefix=f"{CACHE_KEY_PREFIX}_single")
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Создать новый платеж",
+        request_body=PaymentSerializer,
+        responses={
+            201: PaymentSerializer(),
+            400: "Неверные данные"
+        },
+        tags=['Платежи'],
+        security=[{'Bearer': []}]
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
